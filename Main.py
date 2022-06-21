554dcce30ad0b93c24f832c8ba46ca153b1453bd
Lok Chan <chan_l@subway.com>
2022-06-21 17:23:33 -0400

    Hackerrank warmups

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
-from Warmup import hello_world
+from Warmup import hello_world, if_else, arithmetic_operators
 
 if __name__ == '__main__':
-    hello_world()
-
-
+    # hello_world()
+    # if_else()
+    arithmetic_operators()
--- Warmup.py
+++ Warmup.py
@@ -1,3 +1,23 @@
 def hello_world():
     print("Hello, World!")
 
+
+def if_else():
+    n = int(input().strip())
+    if n % 2 == 1:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+
+def arithmetic_operators():
+    a = int(input())
+    b = int(input())
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
